1c76b6f76466dfe2e4b9cc41d00f5e59067b47bc
Yoshiaki Hidai <hidai.620@gmail.com>
2018-02-01 22:47:51 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './style.css';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+
+mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/yhidai/cjd46prjm50b22rpboryo1vpz'
+});
+
+
+map.on('click', function (e) {
+
+    let features = map.queryRenderedFeatures(e.point, {
+        layers: ['chicago-parks'] // replace this with the name of the layer
+    });
+    console.log(features);
+
+    if (!features.length) {
+        return;
+    }
+
+    let feature = features[0];
+    console.log(feature);
+
+    let popup = new mapboxgl.Popup({offset: [0, -15]})
+        .setLngLat(feature.geometry.coordinates)
+        .setHTML(`<h3> ${feature.properties.title}</h3><p>${feature.properties.description}</p>`)
+        .addTo(map);
+});
